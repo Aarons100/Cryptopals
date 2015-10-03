1cd2757ddd981f47235f4264fa2560766bb04751
aarons100 <sedlaa@rpi.edu>
2015-10-03 01:56:52 -0400

    solved Set 1 chall 4

--- Set_1/chall4.c
+++ Set_1/chall4.c
@@ -23,7 +23,7 @@ int atoh_inst(char input){
 		return foo;
 	}
 }
-
+//wrapper for atoh_inst
 char * atoh(char * input){
 	char * result = malloc(strlen(input)/2);
 
@@ -31,50 +31,64 @@ char * atoh(char * input){
 		result[i/2] = (atoh_inst(input[i]) << 4) + atoh_inst(input[i+1]);
 		i++;
 	}
-
 	return result;
 }
-
+//xor's two arrays of bytes
 char * xor(char * input1, char * input2, int size) {
 
 	char * result = malloc(size);
 	for (int i = 0; i < size; i++){
 		result[i] = input1[i] ^ input2[i];
-
 	}
 	return result;
 }
-
+//single byte xor an array
 char * single_byte_xor(char byte, char * input, int size) {
 
 	char * result = malloc(size);
 	for (int i = 0; i < size; i++){
 		result[i] = input[i] ^ byte;
-
 	}
 	return result;
 }
 
 int main(int argc, char * argv[]) {
 	
-	char * input1 = argv[1];
+	    FILE * fp;
+       char * line = NULL;
+       size_t len = 0;
+       ssize_t read;
 
-	int size = strlen(input1);
+       for(int i = 0; i < 0xff; i++){
 
-	char * hex1 = atoh(input1);
+       fp = fopen("./chall4_encoded.txt", "r");
 
-	char * result;
 
-	for(int i = 0; i < 0xff; i++){
+       if (fp == NULL)
+           exit(EXIT_FAILURE);
 
-		result = single_byte_xor(i,hex1,size);
+       while ((read = getline(&line, &len, fp)) != -1) {
 
-		printf("%s\n",result);
+       		int size = strlen(line);
 
+			char * hex1 = atoh(line);
+
+			char * result;
+
+			
+			result = single_byte_xor(i,hex1,size);
+
+
+				printf("%s\n",result);
+			
+				
+		}
+		 fclose(fp);
 	}
-
-
-
-
+		
+      
+       if (line)
+           free(line);
+       exit(EXIT_SUCCESS);
 	return 0;
 }
