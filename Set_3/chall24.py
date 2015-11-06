8e51ed4b3d1fb4f03f3ce07616207205d610ad0a
aarons100 <sedlaa@rpi.edu>
2015-11-05 20:19:28 -0500

    added chall 24 sol

---
+++ Set_3/chall24.py
@@ -0,0 +1,87 @@
+import struct
+import binascii
+import random
+import os
+import time
+import math
+
+def _int32(x):
+    # Get the 32 least significant bits.
+    return int(0xFFFFFFFF & x)
+
+class MT19937:
+
+    def __init__(self, seed):
+        # Initialize the index to 0
+        self.index = 624
+        self.mt = [0] * 624
+        self.mt[0] = seed  # Initialize the initial state to the seed
+        for i in range(1, 624):
+            self.mt[i] = _int32(
+                1812433253 * (self.mt[i - 1] ^ self.mt[i - 1] >> 30) + i)
+
+    def extract_number(self):
+        if self.index >= 624:
+            self.twist()
+
+        y = self.mt[self.index]
+
+        # Right shift by 11 bits
+        y = y ^ y >> 11
+        # Shift y left by 7 and take the bitwise and of 2636928640
+        y = y ^ y << 7 & 2636928640
+        # Shift y left by 15 and take the bitwise and of y and 4022730752
+        y = y ^ y << 15 & 4022730752
+        # Right shift by 18 bits
+        y = y ^ y >> 18
+
+        self.index = self.index + 1
+
+        return _int32(y)
+
+    def twist(self):
+        for i in range(0, 624):
+            # Get the most significant bit and add it to the less significant
+            # bits of the next number
+            y = _int32((self.mt[i] & 0x80000000) +
+                       (self.mt[(i + 1) % 624] & 0x7fffffff))
+            self.mt[i] = self.mt[(i + 397) % 624] ^ y >> 1
+
+            if y % 2 != 0:
+                self.mt[i] = self.mt[i] ^ 0x9908b0df
+        self.index = 0
+
+def MT_Encrypt(ptxt,key):
+
+    twister = MT19937(key)
+    result = ""
+    for i in xrange(0,len(ptxt)):
+        result += chr(ord(struct.pack("<I",twister.extract_number())[0]) ^ ord(ptxt[i]))
+
+    return result
+
+def check_timeseed(ctxt):
+    foo = time.time()
+
+    for i in xrange(int(math.ceil(time.time())),0,-1):
+        maybe_ptxt = MT_Encrypt(ctxt,i)
+        if (maybe_ptxt.find("AAAAAAAAAAAAAA") != -1):
+            print "found key: ", hex(i),"in",time.time() - foo, "seconds"
+            break
+
+key = int(math.floor(time.time()))
+prepend = random.randint(0,64)
+ptxt = os.urandom(prepend) + "AAAAAAAAAAAAAA"
+foo = time.time()
+ctxt = MT_Encrypt(ptxt,key)
+time.sleep(3)
+
+check_timeseed(ctxt)
+"""
+for i in xrange(0,0xFFFF):
+   # print "trying: " , hex(i)
+    maybe_ptxt = MT_Encrypt(ctxt,i)
+    if (maybe_ptxt.find("AAAAAAAAAAAAAA") != -1):
+        print "found key: ", hex(i),"in",time.time() - foo, "seconds"
+        break
+"""
