ca74dab6b7340348aaabf4f44e050ad274663c25
aarons100 <sedlaa@rpi.edu>
2015-11-05 19:29:14 -0500

    implemented my own CTR mode AES

--- Set_3/chall18.py
+++ Set_3/chall18.py
@@ -5,7 +5,9 @@ import sys
 import os
 import binascii
 import struct
-
+import math
+#pycrypto wrappers
+"""
 def CTR_Encrypt(ptxt, key,secret):
 	cipher = AES.new(key,AES.MODE_CTR, counter = lambda: secret)
 	return cipher.encrypt(ptxt)
@@ -17,10 +19,30 @@ def CTR_Decrypt(ctxt,key):
 		AES_CTR = AES.new(key,AES.MODE_CTR, counter = lambda: secret)
 		result += AES_CTR.decrypt(ctxt)[i*16:(i+1)*16]
 	return result
+"""
+
+def CTR_Enc(ptxt,key,nonce):
+	keystream = ""
+	cipher = AES.new(key,AES.MODE_ECB)
+	block_len = int(math.ceil(len(ptxt)/16))
+	if len(ptxt) % 16:
+		block_len += 1
+	print "block count:", block_len
+
+	for i in xrange(0,block_len):
+
+		target = nonce + struct.pack("<I",i) + "\x00"*4
+		#print binascii.hexlify(target) 
+		keystream += cipher.encrypt(target)
+	result = ""
+	for i in xrange(0,len(ptxt)):
+		result += chr(ord(ptxt[i]) ^ ord(keystream[i]))
+
+	return result
 
 string = "L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ=="
 string = base64.b64decode(string)
-
+"""
 secret = "\x00"*8 + "\x03" + "\x00"*7
 key = "YELLOW SUBMARINE"
 
@@ -29,3 +51,8 @@ ptxt = CTR_Decrypt(string,key)
 print ptxt
 
 test = CTR_Encrypt()
+"""
+key = "YELLOW SUBMARINE"
+nonce = "\x00"*8
+
+print CTR_Enc(string,key,nonce)
